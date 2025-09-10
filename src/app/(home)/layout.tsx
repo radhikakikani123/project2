ee3ba3e414a7b52e3a7d6b339644ea400783331e
S R Jay <25se02ce041@ppsu.ac.in>
2025-09-10 10:22:46 +0530

    add footer

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,4 +1,5 @@
 import { Navbar } from "./navbar";
+import { Footer } from "./footer";
 
 interface Props{
     children: React.ReactNode
@@ -7,7 +8,10 @@ const Layout = ({ children }: Props) => {
     return (
         <div className="flex flex-col min-h-screen">
             <Navbar />
-            {children}
+            <div className="flex-1 bg-[#f4f4f0]">
+                {children}
+            </div>
+            <Footer />
         </div>
     )
 }
--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -16,8 +16,8 @@ function Progress({
       className={cn(
         "relative h-3 w-full overflow-hidden rounded-full",
         // modified classes
-        "border bg-transparent",
-        className
+        "border bg-white",
+        className 
       )}
       {...props}
     >
